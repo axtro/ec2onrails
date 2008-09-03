979c4ffa84c7d3d3454e8707a821cd64dc8e2353
Henning Kiel <henning.kiel@gmail.com>
2008-09-03 15:45:23 +0200

    Added scripts to backup and restore files/whole directories to timestamped S3 directories.

---
+++ server/files/usr/local/ec2onrails/bin/backup_files.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/ruby
+
+# Tars a given file or directory and uploads it to S3 using a time-stamped name.
+# Takes 2 required and 1 optional flags:
+#   -sourceFileOrDir  The full path to the file or dir to be saved to S3
+#   -dir              The directory inside the bucket to save above file/dir to
+#   -bucket           [optional] The bucket where <dir> will be created. If this is not specified, it defaults to the bucket_base_name from s3.yml concatenated with the hostname.
+
+exit unless File.exists?("/mnt/app/current")
+
+require "rubygems"
+require 'zlib'
+require 'archive/tar/minitar'
+include Archive::Tar
+require "optiflag"
+require "fileutils"
+require "#{File.dirname(__FILE__)}/../lib/s3_helper"
+require "#{File.dirname(__FILE__)}/../lib/utils"
+
+module CommandLineArgs extend OptiFlagSet
+  flag "sourceFileOrDir"
+  flag "dir"
+  optional_flag "bucket"
+
+  and_process!
+end
+
+# include the hostname in the bucket name so test instances don't accidentally clobber real backups
+bucket = ARGV.flags.bucket
+dir = ARGV.flags.dir
+@s3 = Ec2onrails::S3Helper.new(bucket, dir)
+@temp_dir = "/mnt/tmp/axtro-backup-#{@s3.bucket}-#{dir.gsub(/\//, "-")}"
+local_file = File.join(@temp_dir, "#{Time.new.strftime('%Y-%m-%d--%H-%M-%S')}.tar")
+if File.exists?(@temp_dir)
+  puts "Temp dir exists (#{@temp_dir}), aborting. Is another backup process running?"
+  exit
+end
+
+begin
+  FileUtils.mkdir_p @temp_dir
+  
+  source_file = ARGV.flags.sourceFileOrDir
+  
+  FileUtils.cd(File.dirname(source_file)) do
+    File.open(local_file, 'wb') { |tar| Minitar.pack(File.basename(source_file), tar) }
+  end
+  
+  @s3.store_file(local_file)
+ensure
+  FileUtils.rm_rf(@temp_dir)
+end
---
+++ server/files/usr/local/ec2onrails/bin/restore_files.rb
@@ -0,0 +1,59 @@
+#!/usr/bin/ruby
+
+# Downloads and untars a backup created with axtro_backup.rb from a given directory from S3.
+# Takes 2 required and 2 optional flags:
+#   -destDir          The full path to the dir where the downloaded file should be extracted
+#   -dir              The directory inside the bucket where the timestamped backups can be found
+#   -timestamp        [optional] Download the given timestamp. If this flag is not given, the most recent timestamp will be downloaded. Timestamps have to be in the following format: YYYY-MM-DD--HH-MM-SS
+#   -bucket           [optional] The bucket where <dir> will be created. If this is not specified, it defaults to the bucket_base_name from s3.yml concatenated with the hostname.
+
+require "rubygems"
+require 'zlib'
+require 'archive/tar/minitar'
+include Archive::Tar
+require "optiflag"
+require "fileutils"
+require "#{File.dirname(__FILE__)}/../lib/s3_helper"
+require "#{File.dirname(__FILE__)}/../lib/utils"
+require 'aws/s3'
+include AWS::S3
+
+module CommandLineArgs extend OptiFlagSet
+  flag "destDir"
+  flag "dir"
+  optional_flag "timestamp"
+  optional_flag "bucket"
+  
+  and_process!
+end
+
+dir = ARGV.flags.dir
+@s3 = Ec2onrails::S3Helper.new(ARGV.flags.bucket, dir)
+@temp_dir = "/mnt/tmp/axtro-backup-#{@s3.bucket}-#{dir.gsub(/\//, "-")}"
+if File.exists?(@temp_dir)
+  puts "Temp dir exists (#{@temp_dir}), aborting. Is another backup process running?"
+  exit
+end
+
+begin
+  FileUtils.mkdir_p @temp_dir
+  
+  if ARGV.flags.timestamp
+    filename = "#{ARGV.flags.timestamp}.tar"
+  else
+    # The user didn't supply a timestamp to load, so get a list of all files inside <bucket>/<dir> and take the youngest timestamp.
+    bucket = Bucket.find(@s3.bucket, {:prefix => dir})
+    names_to_buckets = bucket.objects.map { |o| {:name => File.basename(o.key), :object => o }}
+    names_to_buckets.sort_by {|a| a[:name]}
+    filename = names_to_buckets.last[:name]
+  end
+  
+  file = "#{@temp_dir}/#{filename}"
+  @s3.retrieve_file(file)
+
+  FileUtils.cd(ARGV.flags.destDir) do
+    Minitar.unpack(file, '.')
+  end
+ensure
+  FileUtils.rm_rf(@temp_dir)
+end
