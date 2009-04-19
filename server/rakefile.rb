26593adab8ed102385e2486b746b9421ae08a660
Henning Kiel <henning.kiel@gmail.com>
2009-04-19 23:36:28 +0200

    Use rake 0.8.3. Fixed instance build script including gem/version Update version.rb to current version.

--- server/rakefile.rb
+++ server/rakefile.rb
@@ -27,7 +27,7 @@
 require "rake/clean"
 require 'yaml'
 require 'erb'
-require "#{File.dirname(__FILE__)}/../gem/lib/ec2onrails/version"
+require "#{File.dirname(__FILE__)}/../lib/ec2onrails/version"
 
 if `whoami`.strip != 'root'
   raise "Sorry, this buildfile must be run as root."
